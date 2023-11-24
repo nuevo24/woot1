9abe2dd686762b21adb802d962b06c3e80c45072
nuevo24 <141083258+nuevo24@users.noreply.github.com>
2023-11-24 12:32:27 -0500

    Update gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -8,7 +8,7 @@ let oi = `*𝙼𝙴𝙽𝚂𝙰𝙹𝙴:* ${pesan}`
 let teks = `*⺀𝙸 𝙽 𝚅 𝙾 𝙲 𝙰 𝙽 𝙳 𝙾 - 𝙶 𝚁 𝚄 𝙿 𝙾⺀*\n\n❏ ${oi}\n\n❏ *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`
 for (let mem of participants) {
 teks += `┣🎃 @${mem.id.split('@')[0]}\n`}
-teks += `└𝐋𝐚𝐯𝐢𝐞𝐣𝐚𝐬𝐜𝐨𝐨𝐥 𝟐𝟒𝐤`
+teks += `└Team Woot`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 }
 handler.help = ['tagall <mesaje>','invocar <mesaje>']
